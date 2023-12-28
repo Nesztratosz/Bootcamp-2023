7be771fabc3588bb2d57cd6c168761866a5fedd1
Nesztratosz <143605715+Nesztratosz@users.noreply.github.com>
2023-12-28 11:23:19 +0000

    Add files via upload

---
+++ Task 1/conversion.py
@@ -0,0 +1,26 @@
+"""
+1 Give in the variables
+    num1 99.23
+    num2 23
+    num3 150
+    string 100
+2 convert numbers 1 by 1
+print all out separate lines
+"""
+
+num1 = 99.23
+num2 = 23
+num3 = 150
+string1 = str("100")
+
+print("Before")
+print(num1)
+print(num2)
+print(num3)
+print(string1)
+
+print("After")
+print(int(num1))
+print(float(num2))
+print(str(num3))
+print(int(string1))
---
+++ Task 1/hello_world.py
@@ -0,0 +1,19 @@
+"""
+Start
+1 Ask the user name
+2 Give them back the name
+
+3 Ask the user age
+4 Give them back their age
+
+5 print out Hello world
+End
+"""
+
+name = input("Please give me your name: ")
+print("Hello "+ name + "!" )
+
+age = input("Could you give me your age? ")
+print(name + " is " + age + " year old ")
+
+print("Hello World!")
